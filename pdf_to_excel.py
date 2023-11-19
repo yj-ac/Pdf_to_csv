3b9312952a761ced38a8812c839270ac3b39d4f8
山口淳 <yamaguchiatsushi@yamaguchiatsushinoMac-mini.local>
2023-11-19 20:05:24 +0900

    third commit

--- pdf_to_excel.py
+++ pdf_to_excel.py
@@ -3,14 +3,18 @@ import tabula
 import streamlit as st
 
 data = st.file_uploader('PDFファイルをアップロードしてください。')
-dfx = tabula.read_pdf('sample_pdf.pdf',lattice=True,pages='all')
-page = st.number_input('変換するページNOを入力してください。',1,len(dfx))
-df = dfx[page-1]
-df
-csv = df.to_csv().encode('SHIFT-JIS')
-# _dl_file = dfx[page].to_excel('output.xlsx')
-st.download_button(label='Data Download', 
-                   data=csv, 
-                   file_name='output.csv',
-                   mime='text/csv',
-                   )
+if data != None:
+    dfx = tabula.io.read_pdf(data,lattice=True,pages='all')
+    page = st.number_input('変換するページNOを入力してください。',1,len(dfx))
+    df = dfx[page-1]
+
+    st.text('PDFをアップロードしてください。')
+
+    df
+    csv = df.to_csv().encode('SHIFT-JIS')
+        # _dl_file = dfx[page].to_excel('output.xlsx')
+    st.download_button(label='Data Download', 
+                        data=csv, 
+                        file_name='output.csv',
+                        mime='text/csv',
+                        )
